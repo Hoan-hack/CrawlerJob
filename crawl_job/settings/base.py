8acd28c51eca517ce8b87d0101021687e58d954a
ImHoan <ngohoan040793@gmail.com>
2020-12-28 17:37:15 +0700

    Initial commit

--- crawl_job/settings/base.py
+++ crawl_job/settings/base.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.core.exceptions'
     # 'django.contrib.admin',
     # 'django.contrib.auth',
     'django.contrib.contenttypes',
