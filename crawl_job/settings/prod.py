7ddf9d2a2adec88e7ddb86fd752b7da548f77404
ImHoan <ngohoan040793@gmail.com>
2020-12-28 16:14:13 +0700

    Initial commit

--- crawl_job/settings/prod.py
+++ crawl_job/settings/prod.py
@@ -27,8 +27,9 @@ DATABASES = {
 
 import dj_database_url
 
-DATABASE_URL = "postgresql://ec2-54-160-133-106.compute-1.amazonaws.com:5432/d6bcc0nounn8lj"
+DATABASE_URL = "postgres://osmlrkzmdcvsha:8b85ebd07decac6f46ac11e148abc1369848ae1d6208c897e60345d1ed3087e5@" \
+               "ec2-54-160-133-106.compute-1.amazonaws.com:5432/d6bcc0nounn8lj"
 
-prod_db = dj_database_url.config(conn_max_age=500)
+prod_db = dj_database_url.config(default=DATABASE_URL)
 DATABASES['default'].update(prod_db)
 
