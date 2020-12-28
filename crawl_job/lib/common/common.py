1d9339436d527b2b0996a2e4fd8dd4991ae73067
ImHoan <ngohoan040793@gmail.com>
2020-12-28 14:21:12 +0700

    Initial commit

--- crawl_job/lib/common/common.py
+++ crawl_job/lib/common/common.py
@@ -3,7 +3,7 @@ import time
 import re
 
 
-_RE_COMBINE_WHITESPACE = re.compile(r"(?a:\s+)")
+# _RE_COMBINE_WHITESPACE = re.compile(r"(?a:\s+)")
 
 from crawl_job.log.logg import logger
 
@@ -48,10 +48,12 @@ def calculate_time(time_stamp):
 
 
 def get_tag_job(tag_name):
-    beauty_tag_name = str(tag_name).strip().replace("\n", " ")
-    beauty_tag_name = _RE_COMBINE_WHITESPACE.sub(" ", beauty_tag_name)
-    beauty_tag_name.split(" ")
-    return beauty_tag_name
+    # beauty_tag_name = str(tag_name).strip().replace("\n", " ")
+    # beauty_tag_name = _RE_COMBINE_WHITESPACE.sub(" ", beauty_tag_name)
+    # beauty_tag_name.split(" ")
+    # return beauty_tag_name
+    sentence = re.sub(r"\s+", " ", tag_name, flags=re.UNICODE)
+    return sentence
 
 
 # def get_tag_job(data):
