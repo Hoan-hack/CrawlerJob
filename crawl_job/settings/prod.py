13db8105eae210ccda490844e39ed35014a650bb
ImHoan <ngohoan040793@gmail.com>
2020-12-28 16:41:47 +0700

    Initial commit

--- crawl_job/settings/prod.py
+++ crawl_job/settings/prod.py
@@ -7,23 +7,23 @@ CRONJOBS = [
     ('*/5 * * * *', 'crawl_job.cron.craw_job_scheduled'),
 ]
 
-DATABASES = {
-    'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'd6bcc0nounn8lj',
-
-        'USER': 'osmlrkzmdcvsha',
-
-        'PASSWORD': '8b85ebd07decac6f46ac11e148abc1369848ae1d6208c897e60345d1ed3087e5',
-
-        'HOST': 'ec2-54-160-133-106.compute-1.amazonaws.com',
-
-        'PORT': '5432',
-
-    }
-}
+# DATABASES = {
+#     'default': {
+#
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#
+#         'NAME': 'd6bcc0nounn8lj',
+#
+#         'USER': 'osmlrkzmdcvsha',
+#
+#         'PASSWORD': '8b85ebd07decac6f46ac11e148abc1369848ae1d6208c897e60345d1ed3087e5',
+#
+#         'HOST': 'ec2-54-160-133-106.compute-1.amazonaws.com',
+#
+#         'PORT': '5432',
+#
+#     }
+# }
 
 import dj_database_url
 
@@ -32,4 +32,5 @@ DATABASE_URL = "postgres://osmlrkzmdcvsha:8b85ebd07decac6f46ac11e148abc1369848ae
 
 # prod_db = dj_database_url.config(default=DATABASE_URL)
 # DATABASES['default'].update(prod_db)
+DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
