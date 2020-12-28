a5a3d19f0053d57e94a80ddca8750de58a103a61
ImHoan <ngohoan040793@gmail.com>
2020-12-28 11:19:00 +0700

    Initial commit

--- crawl_job/settings/dev.py
+++ crawl_job/settings/dev.py
@@ -10,7 +10,7 @@ CRONJOBS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
