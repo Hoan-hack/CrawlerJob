bfae6e334745cc183c1e1deae39bd65cbcf5ac1d
ImHoan <ngohoan040793@gmail.com>
2020-12-29 15:14:48 +0700

    Removing ignored files

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: gunicorn crawl_job.wsgi --log-file -
-cron: python crawl_job/cron.py
--- staticfiles/js/detail.7a886f392a03.js
+++
@@ -1,9 +0,0 @@
-function createElementFromHTML() {
-    return document.getElementsByClassName('content')[0].innerHTML;
-}
-
-function addElement() {
-    var element = createElementFromHTML();
-    document.getElementsByClassName('content-vew')[0].innerHTML = element.replace(/&amp;/g, "&").replace(/&lt;/g, "<").replace(/&gt;/g, ">");
-}
-addElement();
