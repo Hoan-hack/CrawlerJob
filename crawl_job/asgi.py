f2e2715c47ed16f828d8a2f6f705816779939e8e
ImHoan <ngohoan040793@gmail.com>
2020-12-29 11:15:16 +0700

    Initial commit

--- crawl_job/asgi.py
+++ crawl_job/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crawl_job.settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crawl_job.settings.prod')
 
 application = get_asgi_application()
--- crawl_job/settings/prod.py
+++ crawl_job/settings/prod.py
@@ -1,8 +1,5 @@
 from .base import *
 DEBUG = False
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", __file__)
-import django
-django.setup()
 
 TIME_ZONE = 'Asia/Ho_Chi_Minh'
 
