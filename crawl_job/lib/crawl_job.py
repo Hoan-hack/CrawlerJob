c406d66470e8719f4cfe2881102deed797ef7acf
ImHoan <ngohoan040793@gmail.com>
2020-12-29 12:15:47 +0700

    Initial commit

--- crawl_job/lib/crawl_job.py
+++ crawl_job/lib/crawl_job.py
@@ -44,7 +44,7 @@ def get_job_title():
                     "update_time": update_time
                     }
                 )
-                list_title = sorted(list_title, key=lambda k: k['job_id'])
+                list_title = sorted(list_title, key=lambda k: k['job_id'], reverse=True)
             except Exception as e:
                 logger.debug(e)
         for job_title in list_title:
